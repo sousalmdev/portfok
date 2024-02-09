28f43c156f75c9951718598322b4b168055da00b
sousalmdev <leandromedeiros690@gmail.com>
2024-02-09 15:36:42 -0300

    removing padding and unnecessary script removing

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,38 +22,3 @@ gatilhoAccor.forEach((trigger) => {
                 container.classList.toggle('dark', darkModeToggle.checked);
             });
         });
-        function showImagesWithDelay() {
-            const images = document.querySelectorAll('.box img');
-            const delay = 0;
-
-            // Mostra cada imagem com um atraso gradativo
-            images.forEach(function(image) {
-                setTimeout(function() {
-                    image.style.opacity = 1;
-                }, delay);
-                delay += 500; // Aumenta o atraso para a próxima imagem
-            });
-        }
-        var box = document.querySelector('.box');
-            box.classList.remove('opened')
-        // Adiciona um ouvinte de evento de clique à imagem .fab
-        document.querySelector('.fab').addEventListener('click', function() {
-            var box = document.querySelector('.box');
-         
-            box.classList.toggle('opened'); // Adiciona ou remove a classe 'opened' da caixa
-            if (box.classList.contains('opened')) {
-                showImagesWithDelay(); // Chama a função para exibir imagens com atraso gradativo quando a caixa é aberta
-            }
-        });
-
-    //HORA DO JSASIUDFAS9HGUASIBNFDG
-    let menuBtns = document.querySelectorAll('.menu-btn');
-        let container2 = document.querySelector('.container')
-    menuBtns.forEach(function(btn) {
-    btn.classList.remove('active');
-    btn.addEventListener('click', function(e) {
-        btn.classList.toggle('active'); 
-    })})
- 
-    ;
-    
